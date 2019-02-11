0a7620138ecaab18850a8a52d63cf5f384346317
gomaariiii <>
2019-02-11 15:40:48 +0900

    create rabbitmq sample

---
+++ src/main/java/jp/co/gomaariiii/example/RabbitmqVerticle.java
@@ -0,0 +1,47 @@
+package jp.co.gomaariiii.example;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.AMQP.Queue.DeclareOk;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+
+public class RabbitmqVerticle extends AbstractVerticle {
+
+  /** キュー名 */
+  private static final String QUEUE_NAME = "queue";
+
+  @Override
+  public void start() throws Exception {
+    HttpServer httpServer = vertx.createHttpServer();
+    httpServer.requestHandler(request -> {
+      HttpServerResponse response = request.response();
+      send();
+      response.putHeader("content-type", "text/html").end("helloworld");
+    }).listen(8080);
+  }
+
+  private void send() {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    factory.setPort(5672);
+    try (Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel()) {
+      
+      channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+      String message = "HelloWorld!!";
+      channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+      System.out.println("send message=" + message);
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (TimeoutException e) {
+      e.printStackTrace();
+    }
+  }
+}
---
+++ src/test/java/jp/co/gomaariiii/example/TestRabbitmqVerticle.java
@@ -0,0 +1,77 @@
+package jp.co.gomaariiii.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.Envelope;
+import com.rabbitmq.client.AMQP.BasicProperties;
+
+import io.vertx.core.Vertx;
+import io.vertx.junit5.Timeout;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+
+@ExtendWith(VertxExtension.class)
+public class TestRabbitmqVerticle {
+
+  /** キュー名 */
+  private static final String QUEUE_NAME = "queue";
+
+  @BeforeEach
+  void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
+    vertx.deployVerticle(new RabbitmqVerticle(), testContext.succeeding(id -> testContext.completeNow()));
+  }
+
+  @Test
+  @DisplayName("Should start a Web Server on port 8080")
+  @Timeout(value = 10, timeUnit = TimeUnit.SECONDS)
+  void start_http_server(Vertx vertx, VertxTestContext testContext) throws Throwable {
+    vertx.createHttpClient().getNow(8080, "localhost", "/", response -> testContext.verify(() -> {
+      assertTrue(response.statusCode() == 200);
+      response.handler(body -> {
+        assertEquals("helloworld", body.toString());
+
+        try {
+          Receive(testContext);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      });
+    }));
+  }
+
+  private void Receive(VertxTestContext testContext) throws Exception {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    factory.setPort(5672);
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+    DefaultConsumer defaultConsumer = new DefaultConsumer(channel) {
+      @Override
+      public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] body)
+          throws IOException {
+        String message = new String(body, "UTF-8");
+        System.out.println(" [x] Received '" + message + "'");
+      }
+    };
+
+    channel.basicConsume(QUEUE_NAME, false, defaultConsumer);
+  }
+}
